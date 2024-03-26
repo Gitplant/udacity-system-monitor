3e6c67f13cf04c1914762f0bd25ad93f86300afb
Author Gitplant <79708230+Gitplant@users.noreply.github.com>
2024-03-26 10:31:03 +0100

    Parse Up Time in seconds

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -70,7 +70,17 @@ vector<int> LinuxParser::Pids() {
 float LinuxParser::MemoryUtilization() { return 0.0; }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  string line;
+  long up_time, idle_time;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> up_time >> idle_time;
+  }
+  return up_time;
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
