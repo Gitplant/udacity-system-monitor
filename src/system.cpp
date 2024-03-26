fe6bec8afb82701369439ac237397b6d1dc6f5ea
Author Gitplant <79708230+Gitplant@users.noreply.github.com>
2024-03-26 11:48:01 +0100

    Parse operator system name

--- src/system.cpp
+++ src/system.cpp
@@ -34,8 +34,8 @@ std::string System::Kernel() { return string(); }
 // DONE: Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// DONE: Return the operating system name
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
